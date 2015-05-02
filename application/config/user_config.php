0e59648b1b2f13e2a37ef3323cd55e17e4418fdf
hemingchen <chmkeily@gmail.com>
2015-05-02 12:29:39 +0800

    提交初始版本

---
+++ application/config/user_config.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['ENABLE_USER_CONFIGS']	= true;
+
+require_once ('biz_config/constants.php');
+require_once ('biz_config/error_code.php');
+require_once ('biz_config/location_def.php');
