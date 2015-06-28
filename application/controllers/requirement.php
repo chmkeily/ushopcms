003dcd7fac14bf0baeb6eb7728eae1a477e7a09d
hemingchen <chmkeily@gmail.com>
2015-06-28 15:28:43 +0800

    +需求

---
+++ application/controllers/requirement.php
@@ -0,0 +1,115 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* @brief 用户需求
+*/
+class Requirement extends CI_Controller {
+	
+	/**
+	* constructor
+	*/
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('requirement_model');
+	}
+	
+	/**
+	* @brief 需求查询
+	*  <pre>
+	*	接受的表单数据：
+	*		start_idx	列表开始下标
+	*		length		页大小/列表长度
+	*		type		需求类型
+	*  </pre>
+	* @return 操作结果
+	*/
+	public function index()
+	{
+		$offset		= trim($this->input->get_post('start_idx', TRUE));
+		$length		= trim($this->input->get_post('length', TRUE));
+		$type		= trim($this->input->get_post('type', TRUE));
+
+		if (!is_numeric($offset))
+		{
+			$offset = 0;
+		}
+
+		if (!is_numeric($length)
+			|| 1 > $length || 20 < $length)
+		{
+			$length = 10;
+        }
+        
+		$this->load->library('auth');
+		$providerid = $this->auth->get_userid();
+		if (null === $providerid)
+		{
+			$_RSP['ret'] = ERR_NO_SESSION;
+			$_RSP['msg'] = 'you have to login first';
+			exit(json_encode($_RSP));
+		}
+
+        $conditions = array();
+        if (!empty($userid))
+        {
+            $conditions['requirement_providerid'] = $userid;
+        }
+        if (!empty($type))
+        {
+            $conditions['requirement_type'] = $type;
+        }
+
+		$requirements = $this->requirement_model->get_requirements($conditions, $length, $offset);
+        $viewdata['requirements'] = $requirements;
+        $this->load->view('requirement_view', $viewdata);
+    }
+
+    /**
+     * @brief 需求详情
+     * <pre>
+     *      rid     需求id
+     * </pre>
+     */
+    function details()
+    {
+        $rid	= trim($this->input->get_post('rid', TRUE));
+
+        if (!is_numeric($rid))
+        {
+            $_RSP['ret'] = -1;
+            $_RSP['msg'] = '不合法的需求id';
+            exit(json_encode($_RSP));
+        }
+
+        $requirement = $this->requirement_model->get_requirement_by_id($rid);
+        if (false === $requirement)
+        {
+            $_RSP['ret'] = -1;
+            $_RSP['msg'] = 'no such requirement';
+            exit(json_encode($_RSP));
+        }
+
+        $viewdata['requirement'] = $requirement;
+        $this->load->view('requirement_details', $viewdata);
+    }
+    
+    /**
+     * @brief 流程处理（重要）
+     * <pre>
+     *  接受的表单数据:
+     *      rid     需求id
+     *      action  操作(接单, 拒绝, 结束, 补充备注等)
+     *      comment 操作备注
+     * </pre>
+     */
+    function process()
+    {
+		$rid	= trim($this->input->get_post('rid', TRUE));
+
+    }
+
+}
+
+/* End of file requirement.php */
+/* Location: ./application/controllers/requirement.php */
--- application/views/requirement_view.php
+++ application/views/requirement_view.php
@@ -9,45 +9,47 @@
 </head>
 <body id="home">
 		<div>
-            <form class="form-horizontal" method="post" action="/provider/coupon">
+            <form class="form-horizontal" method="post" action="/provider/requirement">
                 <fieldset>
 				<div class="control-group">
 					<label class="control-label">标题</label>
                     <div class="controls">
                         <input type="text" name="kw_title" class="inputbox" value=""/>
 						<button type="submit" class="btn btn-primary">查询</button>
-						<a class="btn btn-primary" href="#">添加</a>
 					</div>
 				</div>
-           
     			</fieldset>
 		    </form>
 
 			<table class="table table-bordered table-striped table-hover table-condensed">
 				<thead style="background: #CCCCCC url(/asset/images/treetable/bg-table-thead.png) repeat-x;">
 					<tr>
-						<th><span>ID</span></th>
-						<th><span>服务商</span></th>
+						<th><span>需求ID</span></th>
 						<th><span>标题</span></th>
-						<th><span>图标</span></th>
-						<th><span>内容</span></th>
-						<th><span>开始时间</span></th>
-						<th><span>过期时间</span></th>
+						<th><span>类型</span></th>
+						<th><span>发布人</span></th>
+						<th><span>发布时间</span></th>
+						<th><span>店铺类型</span></th>
+						<th><span>店铺城市</span></th>
+						<th><span>预算</span></th>
 						<th><span>状态</span></th>
+						<th><span>服务商</span></th>
 					</tr>
 				</thead>
-			<?php if(isset($coupons)): ?>
+			<?php if(isset($requirements)): ?>
 				<tbody>
-            <?php foreach ($coupons as $coupon): ?>
-                    <tr id="u<?php echo $coupon['coupon_id']; ?>">
-                        <td><?php echo $coupon['coupon_id']; ?></td>
-                        <td><?php echo $coupon['coupon_providerid']; ?></td>
-                        <td><?php echo $coupon['coupon_title']; ?></td>
-                        <td><img style="width=20px;height=20px;" src="<?php echo $coupon['coupon_icon']; ?>"/></td>
-                        <td><?php echo substr($coupon['coupon_content'], 0, 20); ?></td>
-                        <td><?php echo $coupon['coupon_begintime']; ?></td>
-                        <td><?php echo $coupon['coupon_endtime']; ?></td>
-                        <td><?php echo $coupon['coupon_status']; ?></td>
+            <?php foreach ($requirements as $requirement): ?>
+                    <tr id="u<?php echo $requirement['requirement_id']; ?>">
+                        <td><a href="/requirement/details?requirement_id=<?php echo $requirement['requirement_id']; ?>"><?php echo $requirement['requirement_id']; ?></a></td>
+                        <td><?php echo $requirement['requirement_title']; ?></td>
+                        <td><?php echo $requirement['requirement_type']; ?></td>
+                        <td><?php echo $requirement['requirement_ownername']; ?></td>
+                        <td><?php echo $requirement['requirement_st']; ?></td>
+                        <td><?php echo $requirement['requirement_shopcate']; ?></td>
+                        <td><?php echo $requirement['requirement_shopcity']; ?></td>
+                        <td><?php echo $requirement['requirement_budget']; ?></td>
+                        <td><?php echo $requirement['requirement_status']; ?></td>
+                        <td><?php echo $requirement['requirement_providerid']; ?></td>
                     </tr>
             <?php endforeach; ?>
                 </tbody>
