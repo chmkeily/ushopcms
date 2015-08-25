04c4a276d893fc33b6b0643a60434a1489029469
hemingchen <chmkeily@gmail.com>
2015-08-25 20:45:58 +0800

    +流程状态控制器

---
+++ application/libraries/WFProvider.php
@@ -0,0 +1,35 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author: hemingchen
+ *
+ */
+class WFProvider extends Workflow
+{
+	/**
+	* 流程状态和操作定义
+	*/
+
+	protected $_state_graph = null;
+	
+	private create_state_graph()
+	{
+		//这里所有数字以后换成宏定义,包括状态(state),角色类型(role),操作类型(cmd)
+		$this->_state_graph = array(
+			STATE_CREATED	=> array(
+								'roles'		=> array(ROLE_ADMIN),
+								'rules'		=> array(
+												CMD_APPROVE	=> STATE_APPROVED,
+												CMD_REJECT	=> STATE_FINISHED,
+											),
+				),
+				
+			STATE_APPROVED	=> array(
+								'roles'		=> array(ROLE_ADMIN, ROLE_USER),
+								'rules'		=> array(
+												CMD_FIN	=> STATE_FINISHED,
+											),
+				),
+		);
+	}
+}
--- application/libraries/Workflow.php
+++ application/libraries/Workflow.php
@@ -6,26 +6,61 @@
  */
 class Workflow
 {
-	protected $_state_graph = null;
+	/**
+	* 角色定义
+	*/
+	const ROLE_ADMIN 	= 1;
+	const ROLE_PROVIDER = 2;
+	const ROLE_USER		= 3;
+
+	/**
+	* 操作定义
+	*/
+	const CMD_APPROVE 	= 1;
+	const CMD_REJECT	= 2;
+	const CMD_PASSTO	= 3;
+
+	/**
+	* 流程状态定义
+	*/
+	const STATE_CREATED 	= 0;
+	const STATE_FINISHED	= 1;
+	const STATE_CLOSED		= 2;
+
+	/**
+	* 提示语定义
+	*/
+	static WORDING_STATES = array(
+		STATE_CREATED 	=> '新创建',
+		STATE_CLOSED	=> '已关闭',
+		STATE_FINISHED	=> '已完成');
+
+
+	/**
+	* 状态转换图
+	*/
+	protected $_transitions = null;
 	
-	private create_state_graph()
+	private create_transitions()
 	{
-		//这里所有数字以后换成宏定义,包括状态(state),角色类型(role),操作类型(cmd)
-		$this->_state_graph = array(
+		$this->_transitions = array(
 			STATE_CREATED	=> array(
-								'roles'		=> array(ROLE_ADMIN),
-								'rules'		=> array(
-												CMD_APPROVE	=> STATE_APPROVED,
-												CMD_REJECT	=> STATE_FINISHED,
-											),
+				array(
+					'role'	=> ROLE_ADMIN,
+					'cmd'	=> CMD_APPROVE,
+					'next'	=> STATE_FINISHED,
 				),
+
+				array(
+					'role'	=> ROLE_ADMIN,
+					'cmd'	=> CMD_REJECT,
+					'next'	=> STATE_CLOSED,
+				),
+			),
+
+			STATE_CLOSED	=> array(
 				
-			STATE_APPROVED	=> array(
-								'roles'		=> array(ROLE_ADMIN, ROLE_USER),
-								'rules'		=> array(
-												CMD_FIN	=> STATE_FINISHED,
-											),
-				),
+			),
 		);
 	}
 }
