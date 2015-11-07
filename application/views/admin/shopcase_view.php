70e1aa3ced63419c6cbbbe55166387dbd16e46aa
hemingchen <chmkeily@gmail.com>
2015-11-08 02:02:42 +0800

    opt

--- application/views/admin/shopcase_view.php
+++ application/views/admin/shopcase_view.php
@@ -16,7 +16,7 @@
                     <div class="controls">
                         <input type="text" name="kw_title" class="inputbox" value=""/>
 						<button type="submit" class="btn btn-primary">查询</button>
-						<a class="btn btn-primary add_btn" tabTitle="添加案例" href="/html/pages/form/shopcase_create.html">添加</a>
+						<!-- a class="btn btn-primary add_btn" tabTitle="添加案例" href="/html/pages/form/shopcase_create.html">添加</a -->
 					</div>
 				</div>
            
@@ -26,7 +26,7 @@
 			<table class="search_table table table-bordered table-striped table-hover table-condensed">
 				<thead style="background: #CCCCCC url(/asset/images/treetable/bg-table-thead.png) repeat-x;">
 					<tr>
-						<!-- th><span>ID</span></th -->
+						<th><span>ID</span></th>
 						<th><span>名称</span></th>
 						<th><span>简介</span></th>
 						<th><span>提交时间</span></th>
@@ -37,8 +37,8 @@
 				<tbody>
             <?php foreach ($shopcases as $shopcase): ?>
                     <tr id="u<?php echo $shopcase['shopcase_id']; ?>">
-                        <!-- td><a href="/shopcase/details?caseid=<?php echo $shopcase['shopcase_id']; ?>"><?php echo $shopcase['shopcase_id']; ?></a></td -->
-                        <td><?php echo $shopcase['shopcase_name']; ?></td>
+                        <td><?php echo $shopcase['shopcase_id']; ?></td>
+                        <td><a href="/shopcase/details?caseid=<?php echo $shopcase['shopcase_id']; ?>" class="js_url"><?php echo $shopcase['shopcase_name']; ?></a></td>
                         <td><?php echo mb_substr($shopcase['shopcase_intro'],0,50); ?></td>
                         <td><?php echo date('Y-m-d H:i:s', $shopcase['shopcase_ctime']); ?></td>
                         <td><?php echo $shopcase['shopcase_status']; ?></td>
